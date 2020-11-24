01687bb41d2acea6b75655865ab4d9747b434ae8
your name git config --global user.name your <federicoflores163@gmail.com>
2020-11-24 16:41:02 -0300

    Start Clase Git

---
+++ clase_git/src/clase_git.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : clase_git.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	setbuf(stdout,NULL);
+	printf("\nHola Mundo!\n");
+	return EXIT_SUCCESS;
+}
